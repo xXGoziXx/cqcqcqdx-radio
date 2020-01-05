ea8f2cb85c2aecb616c67dd9099f335725c8c22f
xXGoziXx <nosarobo@gmail.com>
2020-01-05 08:19:55 +0000

    global style change to grid container, npm installed slick-carousel, changed images on home to orbit & slick carousel, added onactivate to router outlet for component initialization, optimized foundation imports, updated firestore rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,34 @@ declare var $: any;
 })
 export class AppComponent implements OnInit {
   title = 'cqcqcqdx-radio';
-  ngOnInit() {
-    $(document).foundation();
+  slickOptions = {
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: 'ease-in-out'
+  };
+  currentSlide = 0;
+  onActivate(event) {
+    $(() => {
+      console.log('hello');
+      $(document).foundation();
+      const carousel = $('.slick-carousel').slick(this.slickOptions);
+      $('.orbit-next').on('click', () => {
+        carousel.slick('slickNext');
+      });
+      $('.orbit-previous').on('click', () => {
+        carousel.slick('slickPrev');
+      });
+      $('.orbit-bullets>button').on('click', event => {
+        carousel.slick('slickGoTo', $(event.target).attr('data-slide'));
+      });
+      $('.slick-carousel').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+        this.currentSlide = currentSlide;
+        $('[data-slide=' + currentSlide + ']').removeClass('is-active');
+        $('[data-slide=' + nextSlide + ']').addClass('is-active');
+      });
+    });
   }
+  ngOnInit() {}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,39 @@
-import { Component, OnInit, HostBinding, Host } from '@angular/core';
-
+import { Component, SecurityContext, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+// import { Orbit } from 'foundation-sites/js/foundation';
+import $ from 'jquery';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  myInterval = 3000;
+  activeSlideIndex = 0;
   radios = [
     {
-      alt: 'radio1',
-      src: '../../assets/img/home/radio1.png'
+      alt: 'ICOM IC-7851',
+      src: '../../assets/img/home/radio1.png',
+      caption: 'ICOM IC-7851'
     },
     {
-      alt: 'radio2',
-      src: '../../assets/img/home/radio2.png'
+      alt: 'Kenwood TS-990',
+      src: '../../assets/img/home/radio2.png',
+      caption: 'Kenwood TS-990'
     },
     {
-      alt: 'radio3',
-      src: '../../assets/img/home/radio3.png'
+      alt: 'Yaesu FTDX9000D',
+      src: '../../assets/img/home/radio3.png',
+      caption: 'Yaesu FTDX9000D'
     }
   ];
-  constructor() {}
+  constructor(sanitizer: DomSanitizer) {
+    this.radios = this.radios.map((radio: any) => ({
+      src: radio.src,
+      alt: radio.alt,
+      caption: sanitizer.sanitize(SecurityContext.HTML, radio.caption)
+    }));
+  }
 
   ngOnInit() {}
 }
